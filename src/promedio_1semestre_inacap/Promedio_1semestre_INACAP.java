b481321a6fc7cb3f6a066d9e03b85c8a098f1a58
Christian Aqueveque <soportes.christian@icloud.com>
2019-09-09 22:48:42 -0300

    Inicio del proyecto

---
+++ src/promedio_1semestre_inacap/Promedio_1semestre_INACAP.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package promedio_1semestre_inacap;
+
+/**
+ *
+ * @author christian_aqueveque
+ */
+public class Promedio_1semestre_INACAP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
